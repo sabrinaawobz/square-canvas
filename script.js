3d806261c881ac31ca8cda8f2f4c97a7f12f57f9
sabrinaawobz <sabrina.a@wobz.com>
2023-01-17 16:15:28 +0100

    add canvas

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+var canvas = document.getElementById('canvas');
+
+var ctx = canvas.getContext('2d'); 
+var ctx2 = canvas.getContext('2d'); 
+
+ctx.fillStyle = 'red';
+ctx.fillRect(0, 0, 300, 100); 
+
+ctx2.fillStyle = 'green';
+ctx2.fillRect(280, 0, 500, 100); 
+
+function drawLine(nbrCarre) {
+   
+      
+}
+
+/*
+
+function drawLine(nbrCarre) {
+
+    
+}*/
+
+
+
+
+
+
+
